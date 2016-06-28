07a53323d2805e75a356e4f244d5282651bad4ab
crlossanchez <juan.c.sanchez.c@gmail.com>
2016-06-27 22:06:42 -0400

    Estructura básica de app con yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name exsysApp
+ * @description
+ * # exsysApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('exsysApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name exsysApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the exsysApp
+ */
+angular.module('exsysApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
